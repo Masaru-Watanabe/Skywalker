b19221fd6aa28b706658cd5f41b96b61e7e2e73e
Masaru-Watanabe <sarumaaaaax@gmail.com>
2017-08-21 19:40:56 +0900

    remember_me done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static#home'
   get '/help', to: 'static#help'
   get '/about', to: 'static#about'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
+  
 
 end
